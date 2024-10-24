949cdb4f12d004cbb06f0ef2acc32f3faac498dc
Mind Drifter <forbl1zzard@mail.ru>
2024-10-24 21:25:15 +1000

    cfg

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add nightfox
+
+  -- { "rebelot/kanagawa.nvim" },
+
+  { "craftzdog/solarized-osaka.nvim" },
+
+  -- Configure LazyVim to load nightfox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "kanagawa-wave",
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
+
